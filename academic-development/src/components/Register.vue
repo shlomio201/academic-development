f0acdc17043c0b84c50bd2ccea7de9e4893cee32
shlomi ohana <shlomio201@gmail.com>
2020-03-20 00:53:50 +0200

    register

---
+++ academic-development/src/components/Register.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Register</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Register',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
